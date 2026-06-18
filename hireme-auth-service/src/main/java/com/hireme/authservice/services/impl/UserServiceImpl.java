30d50271991a943474b6c869ddbf07aa4dbc527d
Nebel <nebel.mass@gmail.com>
2026-06-18 15:30:37 +0200

    chore: downgrade Spring Boot and Cloud versions, refactor dependencies, and update healthcheck command

--- hireme-auth-service/src/main/java/com/hireme/authservice/services/impl/UserServiceImpl.java
+++ hireme-auth-service/src/main/java/com/hireme/authservice/services/impl/UserServiceImpl.java
@@ -31,7 +31,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
